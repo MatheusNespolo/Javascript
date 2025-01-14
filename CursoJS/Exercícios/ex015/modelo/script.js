ac13d81c436d284a30bc92e86a31aa7d2594e70a
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-14 11:21:57 -0300

    finalizado ex15
    Falta melhorar as imagens. Commit e era isso!

--- CursoJS/Exercícios/ex015/modelo/script.js
+++ CursoJS/Exercícios/ex015/modelo/script.js
@@ -11,11 +11,40 @@ if (anoNasc.value.length == 0 || anoNasc.value > ano){
     var idade = ano - Number(anoNasc.value)
     res.innerHTML = `Idade calculada: ${idade}`
     var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     if (sexSec[0].checked) {
         genero = 'homem'
+        if (idade >= 0 && idade < 10) {
+            // Criança
+            img.setAttribute('src', 'criancaM.jpg')
+        } else if (idade >= 10 && idade < 25) {
+            // Jovem
+            img.setAttribute('src', 'jovemM.jpg')
+        } else if (idade >= 25 && idade < 50) {
+            // Adulto
+            img.setAttribute('src', 'adultoM.jpg')
+        } else {
+            // Idoso
+            img.setAttribute('src', 'idosoM.jpg')
+        }
     } else if (sexSec[1].checked) {
         genero = 'mulher'
+        if (idade >= 0 && idade < 10) {
+            // Criança
+            img.setAttribute('src', 'criancaF.jpg')
+        } else if (idade >= 10 && idade < 25) {
+            // Jovem
+            img.setAttribute('src', 'jovemF.jpg')
+        } else if (idade >= 25 && idade < 50) {
+            // Adulto
+            img.setAttribute('src', 'adultoF.jpg')
+        } else {
+            // Idoso
+            img.setAttribute('src', 'idosoF.jpg')
+        }
     }
     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
     }
 }
