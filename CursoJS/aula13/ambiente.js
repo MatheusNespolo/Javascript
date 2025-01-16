3f7031daaac5efaec44928ed38aa47afd1c75c22
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-16 09:05:26 -0300

    adicionei mais uma repetição
    Próxima aula. Commit e era isso!

--- CursoJS/aula13/ambiente.js
+++ CursoJS/aula13/ambiente.js
@@ -1,8 +1,13 @@
 var c = 1
-while (c <= 6) {
-    console.log(`Passo ${c}`)
+do {
+    console.log(`Passo ${c}`)       // Template String
     c++
-}
+} while (c <= 10)                   // Enquanto c for menor ou igual a 10, faça
+
+// while (c <= 6) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
 
 // console.log('Tudo bem?')
 // console.log('Tudo bem?')
