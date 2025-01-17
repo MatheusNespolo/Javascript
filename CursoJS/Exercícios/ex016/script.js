83a14603ef0e197a50064a8851bf121b10938ed6
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-17 13:40:29 -0300

    Atualização ex016
    Comentários. Commit e era isso!

--- CursoJS/Exercícios/ex016/script.js
+++ CursoJS/Exercícios/ex016/script.js
@@ -3,13 +3,13 @@ function contar() {
     var end = document.getElementById('fim')
     var step = document.getElementById('passo')
 
-    if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0) {
+    if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0) { //Verificação de campos vazios
         res.innerHTML = 'Impossível contar!'
         alert('[ERRO] Preencha as entradas!')
     } else {
         res.innerHTML = 'Contando: <br>'
         var i = Number(start.value)
-        var f = Number(end.value)
+        var f = Number(end.value)   //Conversão de string para número
         var p = Number(step.value)
         if (p <= 0) {
             alert('Passo inválido! Considerando PASSO 1')
@@ -17,12 +17,12 @@ function contar() {
         }
         if (i < f) {
             // Contagem crescente
-            for (var c = i; c <= f; c += p) {
+            for (var c = i; c <= f; c += p) {   // Começo menor que o final para teste
                 res.innerHTML += `${c} \u{1F449}`
             }
         } else {
             // Contagem regressiva
-            for (var c = i; c >= f; c -= p) {
+            for (var c = i; c >= f; c -= p) {   // Começo maior que o final para teste
                 res.innerHTML += `${c} \u{1F449}`
             }
         }
