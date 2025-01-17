1a7b28d925b00ccc7e60b55504e8adb91159e42a
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-17 14:58:43 -0300

    terminei o ex017
    Vo comer. Tabuadinha foi leve. Commit e era isso!

--- CursoJS/Exercícios/ex017/script.js
+++ CursoJS/Exercícios/ex017/script.js
@@ -1,1 +1,14 @@
-alert('Olá, Matheus!'); // Exibe um alerta com a mensagem "Olá, Matheus!".
+function tabuada() {
+    var num = document.getElementById('numero')
+    var res = document.getElementById('resultado')
+
+    if (num.value.length == 0) {    // Verificação de campo vazio
+        res.innerHTML = 'Impossível calcular a tabuada! Digite um número.'
+    } else {
+        res.innerHTML = ''  // Limpa o campo de resultado para a nova tabuada
+        var n = Number(num.value)   // Conversão de string para número
+        for (var i = 1; i <= 10; i++) {
+            res.innerHTML += `${n} x ${i} = ${n * i}\n`
+        }
+    }
+}
