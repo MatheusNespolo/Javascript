8ecb8d199b6de163baa697ae413ee07b314fbf94
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-21 17:24:48 -0300

    estudo da aula 15
    Commit e era isso!

---
+++ CursoJS/aula015/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+console.log(`Vetor inicial: ${num}`)
+
+num[5] = 6
+
+num.push(7)
+
+console.log(`O vetor agora tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+num.sort()
+console.log(`Com a ordenação, o primeiro valor do vetor  agora é ${num[0]}`)
+console.log(num)
