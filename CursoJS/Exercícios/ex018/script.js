e8c4893bd49c92277b3c8691e78a0e1d8dcc1664
MatheusNespolo <nespolo.m1@gmail.com>
2025-01-24 17:00:05 -0300

    alterações simples
    Ainda não funcional. Commit e era isso!

--- CursoJS/Exercícios/ex018/script.js
+++ CursoJS/Exercícios/ex018/script.js
@@ -3,10 +3,8 @@ function adicionar() {
     var updt = document.getElementById('atualizado')
     let lista = []
 
-    if ((Number(num.value) in lista)) {
-        alert('Número já adicionado!')
-    } else if (num.value.length == 0) {
-        alert('Por favor, insira um número!')
+    if (Number(num.value) < 1 || Number(num.value) > 100) {
+        alert('Por favor, insira um número entre 1 e 100!')
     } else {
         lista.push(Number(num.value))
         updt.innerHTML += `O número ${num.value} foi adicionado.\n`
@@ -14,5 +12,16 @@ function adicionar() {
 }
 
 function finalizar() {
+    var num = document.getElementById('numero')
+    var res = document.getElementById('resultado')
+    let lista = []
 
+    if (lista.length == 0) {
+        alert('Por favor, adicione um número antes de finalizar!')
+    } else {
+        res.innerHTML += `O maior número é ${Math.max.apply(null, lista)}.\n`
+        res.innerHTML += `O menor número é ${Math.min.apply(null, lista)}.\n`
+        res.innerHTML += `A soma dos números é ${lista.reduce((a, b) => a + b, 0)}.\n`
+        res.innerHTML += `A média dos números é ${lista.reduce((a, b) => a + b, 0) / lista.length}.\n`
+    }
 }
