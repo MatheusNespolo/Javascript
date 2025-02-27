842b82a6c6a1d935aead5ec627321e2098317576
MatheusNespolo <nespolo.m1@gmail.com>
2025-02-27 14:51:12 -0300

    níveis de pH
    Commit e era isso!

---
+++ Codedex/pH-Levels.js
@@ -0,0 +1,11 @@
+// pH Levels
+
+let pH = Math.random(0, 14);
+
+if (pH > 7) {
+    console.log('Básico');
+ } else if (pH < 7 ) {
+    console.log('Ácido');
+ } else {
+    console.log('Neutro');
+ }
