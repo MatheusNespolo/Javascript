2043ef8c84e799400300392d559d60e1a7dbeafa
MatheusNespolo <nespolo.m1@gmail.com>
2025-03-07 08:37:35 -0300

    finalizado qualidade do ar
    Commit e era isso!

--- Codedex/air-quality-index.js
+++ Codedex/air-quality-index.js
@@ -1,2 +1,15 @@
 // Air Quality Index
 
+const aqi = 21
+
+if (aqi > 0 && aqi <=50) {
+    console.log('Boa');
+} else if (aqi <= 100) {
+    console.log('Moderada');
+} else if (aqi <= 150) {
+    console.log('Ruim');
+} else if (aqi <= 200) {
+    console.log('Muito Ruim');
+} else if (aqi <= 300) {
+    console.log('Danoso à Saúde');
+}
